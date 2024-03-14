76cedafd5eaa32ff4c6b02b3f786cb7eac0784b6
louisedml <louise.dumoulin62@gmail.com>
2024-03-14 13:14:55 +0100

    premier ajout

---
+++ Seance 1/Seance1.py
@@ -0,0 +1,38 @@
+#EXO 1
+import copy
+tirage=['a','r','b','g','e','s','c','j']
+def pluslongmot(tirage):
+    f=open("frenchssaccent.dic",'r')
+    lexique=[]
+    for ligne in f: #On convertit en liste
+        lexique.append(ligne[0:len(ligne)-1])
+    f.close()
+    mots=[]
+    for L in lexique: #On parcourt les mots qui sont dans la liste
+        copie=copy.deepcopy(tirage)
+        a=1
+        for x in L: #Pour chaque mot, on parcourt les lettres qui le compose
+            if x in copie:
+                copie.remove(x) #si la lettre est dans notre tirage, on la retire car on ne peut plus la réutiliser
+            else:
+                a=0 #si la lettre n'y est pas on change la valeur de a, de sorte que le mot ne puisse pas être pris
+        if a==1:
+            mots.append(L)
+    mots.sort(key=len)
+    points=[]
+    for m in mots:
+        points.append(score(m))
+        maximum=max(points)
+    return mots[-1], maximum, mots[points.index(maximum)] #la fonction retourne le mot le plus long , le maximum de points et le mot associé
+
+#EXO 3
+
+valeur={'a':1,'e':1,'i':1,'l':1,'n':1,'o':1,'r':1,'s':1,'t':1,'u':1,'d':2,'g':2,'m':2,'b':3,'c':3,'p':3,'f':4,'h':4,'v':4,'j':8,'q':8,'k':10,'w':10,'x':10,'y':10,'z':10}
+def score(mot):
+    s=0
+    for x in mot:
+        s=s+valeur[x]
+    return s
+        
+#EXO 4
+        
