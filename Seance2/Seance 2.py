1f7dd586955cd55127a9e686f36e1580aebad1fe
louisedml <louise.dumoulin62@gmail.com>
2024-03-21 13:38:46 +0100

    fin du TD2

--- Seance2/Seance 2.py
+++ Seance2/Seance 2.py
@@ -7,10 +7,13 @@ class Fraction:
         self.num=a
         self.den=b
     def add(self,self2):
+        """additionne deux fractions"""
         return Fraction(self.num*self2.den + self2.num*self.den, self.den*self2.den)
     def mult(self,self2):
-        return Fraction(self.num*self2.num, self.den*self2.den)
+        """multiplie deux fractions"""
+        return Fraction(self.num*self2.num, self.den*self2.den).simplify()
     def simplify(self):
+        """Simplifie une fraction en cherchant le dénominateur commun du numérateur et du dénominateur"""
         return Fraction(self.num//gcd(self.num,self.den),self.den//gcd(self.num,self.den))
         
     def toString(self):
@@ -25,7 +28,8 @@ if __name__=='__main__':
     print('somme',s.toString())
     m=f.mult(g)
     print('multiplication', m.toString())
-    k=m.simplify()
+    q=Fraction(75,5)
+    k=q.simplify()
     print('simplification', k.toString())
     
 #exercice 2
@@ -39,7 +43,7 @@ def H(n):
         h=h.simplify()
     return h.toString()
 
-print(H(10000))
+print(H(1000))
         
 #exercice 4
 
@@ -50,7 +54,7 @@ def leibnitz(n):
         s.simplify()
     return s.toString()
 
-print(leibnitz(10000))
+print(leibnitz(1000))
 
 #exercice 5
 
@@ -59,15 +63,60 @@ class polynomial:
     def __init__(self,L):
         self.coeff=L
     def __str__(self):
-        P=f"("
-        for k in range(len(self.coeff)-1):
-            P.append(f"{self.coeff[k]}*X**{k}")
-        P.append(f")")
+        if self.coeff[0]!=0:
+            P=f"({self.coeff[0]}+"
+            if self.coeff[1]!=0:
+                P=P+f"{self.coeff[1]}*X+"
+        else:
+            if self.coeff[1]!=0:
+                P=f"({self.coeff[1]}*X+"
+            else:
+                P=f"(" 
+        for k in range(2,len(self.coeff)-1):
+            if self.coeff[k]!=0:
+                P=P+f"{self.coeff[k]}*X**{k}+"
+        P=P+f"{self.coeff[len(self.coeff)-1]}*X**{len(self.coeff)-1})"
         return P
     def add(self,self2):
-        m=max(len(self.coeff,self2.coeff))
+        """Prend en argument un deuxième polynôme et renvoit la somme des deux"""
+        m=min(len(self.coeff),len(self2.coeff))
+        L=[]
+        for k in range(m):
+            L=L+[self.coeff[k]+self2.coeff[k]]
+        if len(self2.coeff)>m:
+            for k in range(m+1, len(self2.coeff)-1):
+                L=L+[self2.coeff[k]]
+        elif len(self.coeff)>m:
+            for k in range(m+1, len(self.coeff)-1):
+                L=L+[self.coeff[k]]
+        return polynomial(L)
+    def deriv(self):
+        """Dérive un polynôme"""
+        L=[]
+        for i in range(1,len(self.coeff)):
+            L=L+[self.coeff[i]*i]
+        return polynomial(L)
+    def integrate(self,constante):
+        """intègre un polynôme"""
+        L=[constante]
+        for i in range(len(self.coeff)):
+            L=L+[self.coeff[i]/(i+1)]
+        return polynomial(L)
+        
+            
         
 if __name__=='__main__':
-    p=polynomial([1,2,3,4])
-    print(p.str())
-    
+    p1=polynomial([0,0,3,4])
+    print('p1',p1)
+    p2=polynomial([5,0,3,4,0,6])
+    print('p2',p2)
+    p3=polynomial([7,8,1,4])
+    print('p3',p3)
+    p4=p2.add(p3)
+    print('somme de p2 et p3',p4)
+    p5=p1.add(p2)
+    print('somme de p2 et p1',p5)
+    p6=p2.deriv()
+    print('p6 dérivée de p2',p6)
+    p7=p6.integrate(5)
+    print('intégration de p6 avec 5 comme cst',p7)
